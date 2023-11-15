b5b71f5adbebb1a40e123c1226b1f30961473be9
NxPKG <116948796+NxPKG@users.noreply.github.com>
2023-11-15 06:34:18 +0600

    Create hcl2.go
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ hcl2.go
@@ -0,0 +1,42 @@
+package parsers
+
+import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+	"github.com/khulnasoft-lab/vulnmap-iac-parsers/terraform"
+)
+
+// ParseHCL2 unmarshals HCL files that are written using
+// version 2 of the HCL language and return parsed file content.
+func ParseHCL2(p []byte, v interface{}) (err error) {
+	result := terraform.ParseModule(map[string]interface{}{
+		"foo.tf": string(p),
+	})
+
+	parsedFiles, ok := result["parsedFiles"]
+	if !ok {
+		return errors.Errorf("no parsed files returned")
+	}
+
+	parsedFilesMap, ok := parsedFiles.(map[string]interface{})
+	if !ok {
+		return errors.Errorf("invalid parsed files format")
+	}
+
+	parsed, ok := parsedFilesMap["foo.tf"]
+	if !ok {
+		return errors.Errorf("parse file")
+	}
+
+	parsedString, ok := parsed.(string)
+	if !ok {
+		return errors.Errorf("invalid parse result type")
+	}
+
+	if err := json.Unmarshal([]byte(parsedString), v); err != nil {
+		return errors.Errorf("unmarshal parse result: %v", err)
+	}
+
+	return nil
+}
